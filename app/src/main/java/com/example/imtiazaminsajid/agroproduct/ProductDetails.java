f5a605a2319ff64197c3deaa52c85906e3e50e5e
Imtiaz Amin Sajid <imtiazaminsajid@gmail.com>
2018-05-08 12:36:18 +0600

    Second Activity

---
+++ app/src/main/java/com/example/imtiazaminsajid/agroproduct/ProductDetails.java
@@ -0,0 +1,29 @@
+package com.example.imtiazaminsajid.agroproduct;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class ProductDetails extends AppCompatActivity {
+
+    ImageView imageView;
+    TextView textViewName;
+    TextView details;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_person_details);
+
+        imageView = findViewById(R.id.SecondImage);
+        textViewName = findViewById(R.id.nameofProduct);
+        details = findViewById(R.id.details);
+
+        Bundle bundle = getIntent().getExtras();
+        if(bundle !=null){
+            
+
+        }
+    }
+}
